22e6fc4bf76b9665333ee17698b9ceb7bf92c595
andreaasprer <140675071+andreaasprer@users.noreply.github.com>
2025-10-11 12:07:32 -0700

    Added NavBar

---
+++ src/components/nav.tsx
@@ -0,0 +1,48 @@
+// components/Navbar.jsx
+import Link from "next/link";
+import { Mail, Github, Linkedin } from "lucide-react";
+
+export const NavBar = () => {
+  return (
+    <nav className="fixed top-0 left-0 w-full bg-background text-foreground">
+      <div className="mx-13 my-5 px-6 py-2">
+        <div className="flex justify-between items-center">
+          {/* Logo/Name */}
+          <Link
+            href="/"
+            className="text-white text-l font-semibold tracking-wide"
+          >
+            Andrea Asprer
+          </Link>
+
+          {/* Socials */}
+          <div className="flex items-center gap-8">
+            <a
+              href="mailto:asprer.andreaa@gmail.com"
+              className="text-white hover:text-orange-500 transition-colors"
+              aria-label="Email"
+            >
+              <Mail />
+            </a>
+            <a
+              href="https://github.com/andreaasprer"
+              target="_blank"
+              className="text-white hover:text-orange-500 transition-colors"
+              aria-label="GitHub"
+            >
+              <Github />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/andrea-asprer-366444325/"
+              target="_blank"
+              className="text-white hover:text-orange-500 transition-colors"
+              aria-label="LinkedIn"
+            >
+              <Linkedin />
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
